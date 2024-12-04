ab91983c95f1346eaefc9fc7d88e7364dc7704af
ptrnk0 <vlad.ptrnko@gmail.com>
2024-12-04 17:47:06 +0200

    Create friend list component

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,15 @@
+const FriendList = ({ friends }) => {
+    return (
+        <ul>
+            {friends.map(friend => {
+                return (
+                    <li key={friend.id}>
+                        <FriendListItem />
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default FriendList;
